a8f8a3def669d33640eb27a02158ebc44cb07e8d
matheusleo <matheusleotrab@gmail.com>
2025-01-30 17:22:30 -0300

    fix: clean main

--- src/main.cpp
+++ src/main.cpp
@@ -12,20 +12,5 @@ int main(int argc, char *argv[])
     system("clear");
     StoreSystem store(argv[1]);
 
-    std::string name = "João Silva";
-    std::string email = "joao@gmail.com";
-    std::string password = "senha123";
-
-    Admin admin(name, email, password);
-
-    if (admin.createNewUser())
-    {
-        std::cout << "Admin adicionado com sucesso!" << std::endl;
-    }
-    else
-    {
-        std::cout << "Erro: Admin já existe ou falha ao abrir o arquivo." << std::endl;
-    }
-
     store.run();
 }
